27989ce3460f432c807f2c29a23bf17269d094d3
iskandarzero <riderzerotg@gmail.com>
2022-10-24 15:28:28 -0300

    feat: ICar interface

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+const carZodSchema = z.object({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+export type ICar = z.infer<typeof carZodSchema>;
