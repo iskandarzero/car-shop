4e01c804d53ab9e53f753dbcc253cbe49d70206a
iskandarzero <riderzerotg@gmail.com>
2022-10-25 02:28:39 -0300

    fix: interfaces

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
-const carZodSchema = VehicleZodSchema.extend({
+export const CarZodSchema = VehicleZodSchema.extend({
   doorsQty: z.number().gte(2).lte(4),
   seatsQty: z.number().gte(2).lte(7),
 });
 
-export type ICar = z.infer<typeof carZodSchema>;
+export type ICar = z.infer<typeof CarZodSchema>;
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const VehicleZodSchema = z.object({
+export const VehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
