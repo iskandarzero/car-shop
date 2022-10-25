273671bc618344a5d608e5947292c2b84c33a142
iskandarzero <riderzerotg@gmail.com>
2022-10-25 02:53:06 -0300

    feat: mocks

---
+++ src/tests/mocks/car.mock.ts
@@ -0,0 +1,19 @@
+export const carMock = {
+  bodyMock: {
+    model: 'Carro rápido',
+    year: 1999,
+    color: 'Vermelho',
+    buyValue: 55555,
+    seatsQty: 7,
+    doorsQty: 4
+  },
+  responseMock: {
+    _id: '4edd40c86762e0fb12000003',
+    model: 'Carro rápido',
+    year: 1999,
+    color: 'Vermelho',
+    buyValue: 55555,
+    seatsQty: 7,
+    doorsQty: 4
+  }
+}
