f798ea015e540fb0d73912ad274553dacfdd67be
iskandarzero <riderzerotg@gmail.com>
2022-10-25 16:53:13 -0300

    fix: error treatment

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -4,20 +4,20 @@ import { ErrorTypes, errorCatalog } from '../errors/catalog';
 
 const errorHandler: ErrorRequestHandler = (err: Error | ZodError, _req, res, _next) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({ message: err.issues });
+    return res.status(400).json({ error: err.issues });
   }
 
   const messageAsErrorType = err.message as keyof typeof ErrorTypes;
   const mappedError = errorCatalog[messageAsErrorType];
 
   if (mappedError) {
-    const { httpStatus, message } = mappedError;
+    const { httpStatus, error } = mappedError;
 
-    return res.status(httpStatus).json({ message });
+    return res.status(httpStatus).json({ error });
   }
   
   console.error(err);
-  return res.status(500).json({ message: 'internal error' });
+  return res.status(500).json({ error: 'internal error' });
 };
 
 export default errorHandler;
