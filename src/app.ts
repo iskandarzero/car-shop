7454c17d3c019f0c5590a4392167d547d861e170
iskandarzero <riderzerotg@gmail.com>
2022-10-25 02:31:47 -0300

    feat: Car routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import carRoute from './routes/CarRoute';
+import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(express.json());
+app.use(carRoute);
+app.use(errorHandler);
 
 export default app;
---
+++ src/routes/CarRoute.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarModel from '../models/CarModel';
+import CarService from '../services/CarService';
+
+const carRoute = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+carRoute.post('/cars', (req, res) => carController.create(req, res));
+
+export default carRoute;
